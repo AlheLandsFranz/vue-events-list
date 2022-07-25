ccd981cb3b57d9e28fce871ce1c09ebd26b77db8
unknown <alhenaplandsman@gmail.com>
2022-07-24 22:06:29 -0300

    routh problem

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL:
-    "https://my-json-server.typicode.com/AlheLandsFranz/vue-events-list",
+  baseURL: "https://my-json-server.typicode.com/AlheLandsFranz/vue-events-list",
   withCredentials: false,
   headers: {
     Accept: "application/json",
